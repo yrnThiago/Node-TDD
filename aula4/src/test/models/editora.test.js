2546bba684ffaf86e3db51656a72b25565818178
yrnThiago <thiagodias_123@hotmail.com>
2023-07-28 13:36:38 -0300

    feat: add mocks, simulation funcs

--- aula4/src/test/models/editora.test.js
+++ aula4/src/test/models/editora.test.js
@@ -1,5 +1,6 @@
 import Editora from './../../models/editora.js';
-
+import { describe, expect, it, jest } from "@jest/globals";
+ 
 describe('Testando o modelo Editora', () => {
     const objetoEditora = {
         nome: 'CDC',
@@ -23,7 +24,7 @@ describe('Testando o modelo Editora', () => {
         });
     });
 
-    it('Deve salvar no BD usando a sintaxa moderna', async () => {
+    it.skip('Deve salvar no BD usando a sintaxa moderna', async () => {
         const editora = new Editora(objetoEditora);
 
         const dados = await editora.salvar();
@@ -31,6 +32,30 @@ describe('Testando o modelo Editora', () => {
         const retornado = await Editora.pegarPeloId(dados.id);
 
         expect(retornado).toEqual(
+            expect.objectContaining({
+                id: expect.any(Number),
+                ...objetoEditora,
+                created_at: expect.any(String),
+                updated_at: expect.any(String),
+            })
+        )
+    });
+
+    it('Deve fazer uma chamada simulada ao BD', () => {
+        const editora = new Editora(objetoEditora);
+
+        editora.salvar = jest.fn().mockReturnValue({
+            id: 10,
+            nome: 'CDC',
+            cidade: 'Sao Paulo',
+            email: 'c@c.com',
+            created_at: '2022-10-01',
+            updated_at: '2022-10-01',
+        });
+
+        const retorno = editora.salvar();
+
+        expect(retorno).toEqual(
             expect.objectContaining({
                 id: expect.any(Number),
                 ...objetoEditora,
