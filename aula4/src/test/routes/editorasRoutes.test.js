001119d95801b67b901e852cf6f2b5212cfd3bd0
yrnThiago <thiagodias_123@hotmail.com>
2023-07-28 13:42:59 -0300

    feat: add hooks

---
+++ aula4/src/test/routes/editorasRoutes.test.js
@@ -0,0 +1,13 @@
+import { afterEach } from '@jest/globals';
+import app from '../../app.js';
+
+let server;
+beforeEach( () => {
+    const port = 3000;
+    server = app.listen(port);
+});
+
+afterEach( () => {
+    server.close();
+})
+
